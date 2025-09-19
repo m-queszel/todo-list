9f420dd408287ef8fc62bb0a997e27faf5636726
queszel <queszeledu@gmail.com>
2025-09-19 15:11:40 -0300

    Fixed spelling errors in About Page

--- src/aboutPage.js
+++ src/aboutPage.js
@@ -9,7 +9,7 @@ export default function initializeAboutPage() {
   const title = document.createElement("h1");
   title.innerHTML = "About This Mini-Project:";
   const text = document.createElement("p");
-  text.innerHTML = "This is just a small project of creating the classic Todo list. Although it isn't necessarily a To-do list, it's a project board that has similar function (e.g., You can create and remove projects and assign due dates to them). The project card will display the inputted deadline, and can then be removed by the user by clicking the X icon. The primary goal of this project was to gain hands-on experience working with localStorage and converting JavaScript objects to be parsed or stringified to/from JSON and placed in localStorage."
+  text.innerHTML = "This is just a small project of creating the classic to-do list. Although it isn't necessarily a to-do list, it's a project board that has similar function (e.g., You can create and remove projects and assign due dates to them). The project card will display the inputted deadline, and can then be removed by the user by clicking the X icon. The primary goal of this project was to gain hands-on experience working with localStorage and converting JavaScript objects to be parsed or stringified to/from JSON and placed in localStorage."
   subContainer.appendChild(title);
   subContainer.appendChild(text);
 
